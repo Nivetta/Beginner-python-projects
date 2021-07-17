67834e604d739254e216e59c888129ec2c3551b2
Nivetta <85820700+Nivetta@users.noreply.github.com>
2021-07-17 19:15:19 +0530

    Add files via upload

---
+++ Snake-Game/food.py
@@ -0,0 +1,24 @@
+from turtle import Turtle
+from random import randint
+
+# class for displaying food
+class Food(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.shape("circle")
+        self.penup()
+        self.color("orange")
+        self.turtlesize(0.5,0.5)
+        self.speed("fastest")
+        self.goto(randint(-280,280),randint(-280,280))
+        self.refresh()
+        
+    def refresh(self):
+        self.goto(randint(-280,280),randint(-280,280))
+        
+        
+        
+        
+        
+        
+
---
+++ Snake-Game/scoreboard.py
@@ -0,0 +1,36 @@
+from turtle import Turtle
+# constants
+FONT = ("Arial",22,"normal")
+ALIGN = "center"
+
+# class for showing, updating, increasing score and changing game status
+class Scoreboard(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.scores = 0
+        self.color("white")
+        self.penup()
+        self.goto(0,267)
+        self.hideturtle()
+        self.update()
+        
+    def update(self):
+        self.write(f"Score : {self.scores}",align=ALIGN,font=FONT)
+        
+    def increase_score(self):
+        self.scores += 1
+        self.clear()        
+        self.update()
+        
+    def game_over(self):
+        self.goto(0,0)
+        self.write("GAME OVER",align=ALIGN,font=FONT)
+        
+        
+        
+        
+    
+        
+        
+        
+        
